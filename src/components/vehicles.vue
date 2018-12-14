be8c55bb35aabef1aaf3bd7d85f11f83510b75f9
wjnowen <862580797@qq.com>
2018-12-14 13:25:17 +0800

    update

--- src/components/vehicles.vue
+++ src/components/vehicles.vue
@@ -1,22 +1,95 @@
 <template>
   <div>
       <h1 align="center"> vehicles </h1>
-    <table class="table table-bordered table-striped text-center"> 
-      <tr>
-        <td>name</td>
-        <td>{{msg.name}}</td>
-      </tr>
-    
-      <tr>
-        <td>films</td>
-         <a v-for="item in msg.films" :key = "item">
-            {{ item }}
-            <br>
-        </a>
-      </tr>
-      
+      <table class="table table-bordered table-striped text-center">
+          <tr>
+              <td>name</td>
+              <td>{{msg.name}}</td>
+          </tr>
 
-    </table>
+          <tr>
+              <td>model</td>
+              <td>{{msg.model}}</td>
+          </tr>
+
+          <tr>
+              <td>manufacturer</td>
+              <td>{{msg.manufacturer}}</td>
+          </tr>
+
+          <tr>
+              <td>cost_in_credits</td>
+              <td>{{msg.cost_in_credits}}</td>
+          </tr>
+
+          <tr>
+              <td>length</td>
+              <td>{{msg.length}}</td>
+          </tr>
+
+          <tr>
+              <td>max_atmosphering_speed</td>
+              <td>{{msg.max_atmosphering_speed}}</td>
+          </tr>
+
+          <tr>
+              <td>crew</td>
+              <td>{{msg.crew}}</td>
+          </tr>
+
+          <tr>
+              <td>passengers</td>
+              <td>{{msg.passengers}}</td>
+          </tr>
+
+          <tr>
+              <td>cargo_capacity</td>
+              <td>{{msg.cargo_capacity}}</td>
+          </tr>
+
+          <tr>
+              <td>consumables</td>
+              <td>{{msg.consumables}}</td>
+          </tr>
+
+          <tr>
+              <td>vehicle_class</td>
+              <td>{{msg.vehicle_class}}</td>
+          </tr>
+
+          <tr>
+              <td>created</td>
+              <td>{{msg.created}}</td>
+          </tr>
+
+          <tr>
+              <td>edited</td>
+              <td>{{msg.edited}}</td>
+          </tr>
+
+          <tr>
+              <td>url</td>
+              <td>{{msg.url}}</td>
+          </tr>
+
+          <tr>
+              <td>pilots</td>
+              <a v-for="item in msg.pilots" :key="item">
+                  {{ item }}
+                  <br>
+              </a>
+          </tr>
+
+          <tr>
+              <td>films</td>
+              <a v-for="item in msg.films" :key="item">
+                  {{ item }}
+                  <br>
+              </a>
+          </tr>
+
+
+      </table>
   </div>
 </template>
 
